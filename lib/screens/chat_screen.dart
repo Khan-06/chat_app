f960ceee363f0e17b01310fd4c42f610d7a66545
Khan-06 <m.abdulbasit.k.06@gmail.com>
2024-01-31 00:51:26 +0500

    There are some errors in this push!

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:js_interop';
+
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -13,6 +16,9 @@ class ChatScreen extends StatelessWidget {
           child: const Text('This kinda works!'),
         ),
       ),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        FirebaseFirestore.instance.collection('chats/nk9kFG6nYJReE6c2Cbpe/message').snapshots().listen((event) {print(event);});
+      }, child: const Icon(Icons.add)),
     );
   }
 }
